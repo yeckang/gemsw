3c7987c6d8ca6d230bd17739aa3bc0c6110f1419
Daniel Estrada <destrada@lxplus736.cern.ch>
2021-11-18 18:05:45 +0100

    GEMCosmicMuonForQC8 was imported and adapted

---
+++ RecoMuon/interface/HeaderForQC8.h
@@ -0,0 +1,10 @@
+#ifndef CosmicMuonProducer_HeaderForQC8_H
+#define CosmicMuonProducer_HeaderForQC8_H
+
+
+#define QC8FLAG_SEEDINFO_MASK_REFVERTROLL18  0x00000001
+
+#define QC8FLAG_SEEDINFO_MASK_DIFFCOL        0x00000006
+#define QC8FLAG_SEEDINFO_SHIFT_DIFFCOL       1
+
+#endif //CosmicMuonProducer_HeaderForQC8_H
